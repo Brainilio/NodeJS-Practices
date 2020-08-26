eff46591a1c7011ed7bd1c9bd6080ddd856d0b59
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-08-26 15:56:21 +0200

    parsed api data

--- node-farm/index.js
+++ node-farm/index.js
@@ -34,6 +34,11 @@ console.log("Reading file...")
 ////////////////////////////////
 // SERVER
 
+//executed only once when we start program
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8")
+const dataObj = JSON.parse(data)
+
 const server = http.createServer((req, res) => {
 	const pathName = req.url
 
@@ -41,6 +46,11 @@ const server = http.createServer((req, res) => {
 		res.end("This is the overview")
 	} else if (pathName === "/product") {
 		res.end("This is the product")
+	} else if (pathName === "/api") {
+		res.writeHead(200, {
+			"Content-Type": "application/json",
+		})
+		res.end(data)
 	} else {
 		//write headers before response
 		res.writeHead(404, {
